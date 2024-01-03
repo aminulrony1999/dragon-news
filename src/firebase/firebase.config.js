2f414ab21d34bc4adf0f4fd1046eff69df06b873
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-03 23:00:31 +0530

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDfTLi9u7Eq1WtY4bAXuYnJ7MefksAp4HY",
+  authDomain: "dragon-news-cd1cd.firebaseapp.com",
+  projectId: "dragon-news-cd1cd",
+  storageBucket: "dragon-news-cd1cd.appspot.com",
+  messagingSenderId: "240842800140",
+  appId: "1:240842800140:web:eeea16daff70678f4f4229",
+};
+const app = initializeApp(firebaseConfig);
