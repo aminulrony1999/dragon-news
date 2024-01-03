ea2c272a7a17106f129e5dbd1a1e98e4986c18cb
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-04 02:59:14 +0530

    date and time format is added

--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
+import moment from 'moment';
 const Header = () => {
     return (
         <nav>
+            <div className="text-center">
             <img src={logo} alt="loading..." />
+            <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
+            <p>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
+            </div>
         </nav>
     );
 };
