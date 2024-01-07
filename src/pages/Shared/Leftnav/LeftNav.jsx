fcbbbbb2d32dcb896580b34a4ae0a0ad7ba90ce4
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-07 15:45:51 +0530

    category shown in the UI

--- src/pages/Shared/Leftnav/LeftNav.jsx
+++ src/pages/Shared/Leftnav/LeftNav.jsx
@@ -1,17 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    const [categories, setCategories] = useState([]);
-    useEffect(()=>{
-        fetch('http://localhost:5000/categories')
-        .then(res => res.json())
-        .then(data => setCategories(data))
-    },[]);
-    return (
-        <div>
-
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.error(error));
+  }, []);
+  return (
+    <div className="ps-4">
+      <h4>All Category</h4>
+      <div>
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link>{category.name}</Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
