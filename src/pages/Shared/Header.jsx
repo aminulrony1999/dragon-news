dd8be721464f1b9f7ef91409b4ca0bb092fd7b25
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-07 07:49:34 +0530

    nav bar added

--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
-import logo from '../../assets/logo.png';
-import moment from 'moment';
-const Header = () => {
-    return (
-        <nav>
-            <div className="text-center">
-            <img src={logo} alt="loading..." />
-            <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
-            <p>{moment().format("dddd, MMMM D, YYYY")}</p>
-            </div>
-        </nav>
-    );
-};
+import React from "react";
+import logo from "../../assets/logo.png";
+import moment from "moment";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
+const Header = () => {
+  return (
+    <Container>
+      <div className="text-center">
+        <img src={logo} alt="loading..." />
+        <p className="text-secondary">
+          <small>Journalism Without Fear or Favour</small>
+        </p>
+        <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+      </div>
+      <div className="d-flex">
+        <Button variant="danger">Latest News</Button>{" "}
+        <Marquee className="text-danger" speed={100} pauseOnHover={true}>
+          Welcome to my Developer Portfolio. I am a new developer. Recently, I
+          have started to learn web development.
+        </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#career">Career</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+              <Button variant="secondary">Log In</Button>{' '}
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
+  );
+};
 export default Header;
