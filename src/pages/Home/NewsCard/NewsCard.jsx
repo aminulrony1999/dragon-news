14d63f79a62c0800a241498d5eba23c45671c565
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-10 00:38:39 +0530

    updated

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,15 +1,31 @@
+import moment from "moment";
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NewsCard = ({ news }) => {
-  const { title, details, _id } = news;
+  const { title, details, _id, image_url, author } = news;
   return (
     <Card className="text-center mb-4">
-      <Card.Header>Featured</Card.Header>
+      <Card.Header className="d-flex">
+        <Image style={{height:"40px"}} src={author.img} roundedCircle />
+        <div className="ps-2">
+            <p className="mb-0">{author?.name}</p>
+            <p><small>{moment(author?.published_date).format('YYYY-MM-D')}</small></p>
+        </div>
+      </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
+        <Card.Img variant="top" src={image_url} />
         <Card.Text>
-            {details}
+          {details.length < 250 ? (
+            <>{details}</>
+          ) : (
+            <>
+              {details.slice(0, 250)}...
+              <Link to={`/news/${_id}`}>Read More</Link>
+            </>
+          )}
         </Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted">2 days ago</Card.Footer>
