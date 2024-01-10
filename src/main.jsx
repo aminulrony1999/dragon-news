bf7d5648b836b001babf0835bf9ba283ceef3eb2
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-10 23:40:22 +0530

    layout updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import Main from "./layout/Main";
 import Home from "./pages/Home/Home/Home";
 import Category from "./pages/Home/Category/Category";
 import NewsLayout from "./layout/NewsLayout";
 import News from "./pages/News/News/News";
 import AuthProvider from "./Provider/AuthProvider";
+import LoginLayout from "./layout/LoginLayout";
+import LogIn from "./pages/LogIn/LogIn";
+import Register from "./pages/LogIn/Register/Register";
 const router = createBrowserRouter([
   {
     path: "/",
+    element : <LoginLayout></LoginLayout>,
+    children : [
+      {
+        path: "/",
+        element : <Navigate to="/category/0"></Navigate>
+      },
+      {
+        path : "login",
+        element: <LogIn></LogIn>
+      },
+      {
+        path : "register",
+        element : <Register></Register>
+      }
+    ]
+  },
+  {
+    path: "category",
     element: <Main></Main>,
     children : [
       {
-        path : "/",
-        element : <Category></Category>,
-        loader : ()=>fetch('http://localhost:5000/news')
-      },
-      {
-        path : 'category/:id',
+        path : ':id',
         element : <Category></Category>,
         loader : ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
       }
