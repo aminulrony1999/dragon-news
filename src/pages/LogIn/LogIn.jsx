5e256b0f10e667064dc86f254caa861f1c786be4
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-11 00:53:52 +0530

    Log In button updated

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
   return (
-    <Container>
+    <Container className="w-25 mx-auto">
+      <h3>Please Log In</h3>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We will never share your email with anyone else.
-          </Form.Text>
+          <Form.Control type="email" name="email" placeholder="Enter email" required />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" name="password" placeholder="Password" required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
@@ -23,6 +22,9 @@ const LogIn = () => {
         <Button variant="primary" type="submit">
           Log In
         </Button>
+        <Form.Text className="text-secondary">Don't have an account? <Link to="/register">Register</Link></Form.Text>
+        <Form.Text className="text-success"> HoiHoiHoi</Form.Text>
+        <Form.Text className="text-danger"> HueHueHue</Form.Text>
       </Form>
     </Container>
   );
--- src/pages/LogIn/Register/Register.jsx
+++ src/pages/LogIn/Register/Register.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-            <h2>This is Register Page</h2>
-        </div>
-    );
+  return (
+    <Container className="w-25 mx-auto">
+      <h3>Please Register</h3>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="confirmpassword"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Sign Up
+        </Button>
+        <Form.Text className="text-secondary">
+          Already have an account? <Link to="/register">Sign In</Link>
+        </Form.Text>
+        <Form.Text className="text-success"> HoiHoiHoi</Form.Text>
+        <Form.Text className="text-danger"> HueHueHue</Form.Text>
+      </Form>
+    </Container>
+  );
 };
 
 export default Register;
