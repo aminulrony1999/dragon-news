5f022b2004bab93f616184c874160cb0ed9a24f7
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-11 17:31:51 +0530

    firebase login added

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
@@ -9,11 +9,15 @@ const AuthProvider = ({ children }) => {
   {
     return createUserWithEmailAndPassword(auth, email, password);
   }
+  const logIn = (email,password) =>{
+    return signInWithEmailAndPassword(auth,email,password);
+  }
   const authInfo = {
     user,
     setUser,
     newUser,
-  };
+    logIn
+  }
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const LogIn = () => {
+  const {setUser, logIn} = useContext(AuthContext);
+  const handleLogIn = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    logIn(email,password)
+    .then(user => {
+      setUser(user);
+      console.log(user);
+      form.reset();
+    })
+    .catch(error =>{
+      console.error(error.message);
+    })
+  }
   return (
     <Container className="w-25 mx-auto">
       <h3>Please Log In</h3>
-      <Form>
+      <Form onSubmit={handleLogIn}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" name="email" placeholder="Enter email" required />
