dc90b4bfd87e7361352eafc3b822d71da53f5fc7
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-17 22:50:44 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import Main from "./layout/Main";
-import Home from "./pages/Home/Home/Home";
 import Category from "./pages/Home/Category/Category";
 import NewsLayout from "./layout/NewsLayout";
 import News from "./pages/News/News/News";
@@ -17,52 +20,63 @@ import Terms from "./pages/termsAndConditions/Terms";
 const router = createBrowserRouter([
   {
     path: "/",
-    element : <LoginLayout></LoginLayout>,
-    children : [
+    element: <LoginLayout></LoginLayout>,
+    children: [
       {
         path: "/",
-        element : <Navigate to="/category/0"></Navigate>
+        element: <Navigate to="/category/0"></Navigate>,
       },
       {
-        path : "login",
-        element: <LogIn></LogIn>
+        path: "login",
+        element: <LogIn></LogIn>,
       },
       {
-        path : "register",
-        element : <Register></Register>
+        path: "register",
+        element: <Register></Register>,
       },
       {
-        path : "terms",
-        element : <Terms></Terms>
-      }
-    ]
+        path: "terms",
+        element: <Terms></Terms>,
+      },
+    ],
   },
   {
     path: "category",
     element: <Main></Main>,
-    children : [
+    children: [
       {
-        path : ':id',
-        element : <Category></Category>,
-        loader : ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
-      }
-    ]
+        path: ":id",
+        element: <Category></Category>,
+        loader: ({ params }) =>
+          fetch(
+            `https://the-dragon-news-server-olive.vercel.app/categories/${params.id}`
+          ),
+      },
+    ],
   },
   {
-    path: 'news',
-    element : <NewsLayout></NewsLayout>,
-    children : [
+    path: "news",
+    element: <NewsLayout></NewsLayout>,
+    children: [
       {
-        path: ':id',
-        element : <PrivateRoute><News></News></PrivateRoute>,
-        loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
-      }
-    ]
-  }
-
+        path: ":id",
+        element: (
+          <PrivateRoute>
+            <News></News>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(
+            `https://the-dragon-news-server-olive.vercel.app/news/${params.id}`
+          ),
+      },
+    ],
+  },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider><RouterProvider router={router} /></AuthProvider>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
